d52871565aaae940a637a70d57ce3440992e1b12
Namecipher <jhaabhishek115@gmail.com>
2022-06-23 09:57:39 +0530

    multiplication logic added

---
+++ multiply.js
@@ -0,0 +1,10 @@
+function multiply(){
+var num1 = document.getELementById("firstno").value
+
+var num2 = document.getElementById("secondno").value
+
+var result = num1 * num2
+
+document.getElementById("result").value=result
+
+}
